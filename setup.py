c7b761c9339523a26f9ffc4c9d79dab140ca5baf
Mohamed Jasim Ahmed <34663944+MJAISE@users.noreply.github.com>
2024-06-27 16:29:19 +0300

    feat: added setup.py - Updated setup.py: This file is used for package distribution. It now includes the necessary dependencies and metadata for the FIB payment SDK. This will ensure that the SDK can be installed and used correctly in other projects.

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="fib-python-payment-sdk",
+    version="0.1.0",
+    description="SDK for integrating with the FIB payment system, enabling user authentication and payment "
+                "transactions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://the-gw.com/",
+    author="Gateway ICT",
+    author_email="info@the-gw.com",
+    license="MIT",
+    keywords=["sdk", "fib", "payment", "integration"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "requests>=2.31.0"
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0.0"
+        ]
+    },
+    python_requires=">=3.7, <4",
+    include_package_data=True,  # This will include all files in MANIFEST.in the package when it is installed
+)
